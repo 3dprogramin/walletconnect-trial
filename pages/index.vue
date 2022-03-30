87970aec68a476d8a90194d8e5c224c8ecfde072
3dprogramin <andrei@3dprogramin.io>
2022-03-30 13:20:24 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <vue-erdjs-connect></vue-erdjs-connect>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
